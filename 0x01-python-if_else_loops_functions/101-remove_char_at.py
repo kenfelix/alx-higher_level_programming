603828fb673eb0a9a6286de933397a38a0c66bf5
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-08 14:58:14 +0000

    advance updates

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in range(0, n):
-        print("{}".format(str[i]), end='')
-    for j in range(n + 1, len(str)):
-        print("{}".format(str[j]), end='')
-
+    if n < 0:
+        return str
+    count = 0
+    str_copy = ""
+    for element in str:
+        if count == n:
+            count += 1
+            continue
+        str_copy += str[count]
+        count += 1
+    return str_copy
