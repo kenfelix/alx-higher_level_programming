25b165ef3d42cf3967458aad9f52ade3753a4318
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-17 18:29:18 +0000

    Updates

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,9 @@
 def weight_average(my_list=[]):
     add = 0
     base = 0
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
     for element in my_list:
-        add += element[0] * element[1]
+        add += (element[0] * element[1])
         base += element[1]
     return (add / base)
