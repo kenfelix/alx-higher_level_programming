354f6d1cc86fe7b178cf4106f988d02247162811
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-27 14:22:44 +0000

    project Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for element in range(x):
         try:
-            print("{}".format(element), end="")
+            print("{}".format(my_list[element]), end="")
             count++
         except: IndexError
             break
