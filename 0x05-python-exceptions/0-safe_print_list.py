b2f60aca564e7dcca1e54e8992838cb7fa36d136
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-27 14:18:02 +0000

    project Update

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for element in range(x):
+        try:
+            print("{}".format(element), end="")
+            count++
+        except: IndexError
+            break
+    print("")
+    return count
