0d6b039c9c1f28ef95583e45db3b57e9550dc52f
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-28 18:41:28 +0000

    New commmit on classes and objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Defines a squre with a private instance attribute called size"""
+    def __init__(self, size=0):
+        """instantializes a privated instance of a sqare"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return (self.__size)**2
