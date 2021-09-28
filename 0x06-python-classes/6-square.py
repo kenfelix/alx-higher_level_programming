543de17a8a703dee460cb32ece7d2ce9bcbb39c4
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-28 22:01:39 +0000

    new commit on classes and objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,10 +35,10 @@ class Square:
         """Gets the position"""
         self.__position = value
 
-        if (not isinstance(size_value, tuple) or
-                len(size_value) != 2 or
-                not all(isinstance(num, int) for num in size_value) or
-                not all(num >= 0 for num in size_value)):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
