10787b9696f4d40de32d3c9334103e0a425c9312
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-28 18:13:01 +0000

    New commmit on classes and objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Defines a squre with a private instance attribute called size"""
+    def __init__(self, size):
+        """instantializes a privated instance of a sqare"""
+        self.__size = size
