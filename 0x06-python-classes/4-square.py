e723aae6f7a19096d52838a7dee702a9dad13461
Emeka Okafor <kmrapper.kf@gmail.com>
2021-09-28 19:40:06 +0000

    New commmit on classes and objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,9 +19,9 @@ class Square:
         """Gets the size"""
         self.__size = value
 
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
