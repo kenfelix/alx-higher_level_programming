328fbf630201ced988a8b0c06506d253a8b7b632
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-04 21:55:15 +0000

    New update on mor classes and objects

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+"""
+
+
+class Rectangle:
+    """A Class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width value"""
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """retrieves the width"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setd the height value"""
+        self.__height = value
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """returns the rectangle area"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        return self.height + self.width
