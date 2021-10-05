23aa18e99c3703231e686cae1aa2307663da106f
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-05 18:11:13 +0000

    New update on More classes and objects

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle by: (based on 2-rectangle.py)
+"""
+
+
+class Rectangle:
+    """A Class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width value"""
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """retrieves the width"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setd the height value"""
+        self.__height = value
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """returns the rectangle area"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2 * (self.height + self.width))
+
+    def __str__(self):
+        """Returns the rectangle with the # character."""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rectangle = []
+        for i in range(self.__height):
+            [rectangle.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rectangle.append("\n")
+        return ("".join(rectangle))
