ac54c72bc56c3204eadd159a36e2dd82c7cc9c70
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-12 13:03:17 +0000

    new commit on python inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Same class module.
-   contains only one function, is_same_class()
+
+   contains only one function, is_same_class().
 """
 
 
 def is_kind_of_class(obj, a_class):
     """checks if the object is an instance of, or a class that inherited from, the specified class """
 
-    return isinstance(obj, a_class)
+    return (isinstance(obj, a_class))
