031b592b44d9147ea3b64de43d52bd8977ab039c
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-12 13:38:15 +0000

    new commit on python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""BaseGeometry Module (based on 5-base_geometry.py).
+Contains a class, BaseGeometry() that raises an exception
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """raises an Exception with the message."""
+        raise("area() is not implemented")
