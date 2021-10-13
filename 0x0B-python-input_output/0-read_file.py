4fd9af0058f7b9d04d2d98812ca4bebed3a7e7bc
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-13 21:53:55 +0000

    New commit for python input/ouput

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ Contains the only one function, read_file().
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, mode="r" encoding="utf-8") as f:
+    with open(filename, 'r') as f:
         print(f.read(), end='')
