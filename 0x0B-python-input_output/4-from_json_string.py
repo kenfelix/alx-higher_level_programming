42bb6d0dd8d145e7724064c570de4a9d8b9f375c
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-14 07:44:40 +0000

    New commit on python input/output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""To Json string to python object module.
+
+Contains one function, from_json_string().
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+    """
+    return json.loads(my_obj)
