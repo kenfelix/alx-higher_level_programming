0e982b63677574c681b220cd170bb94e38a0bc4e
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-14 09:05:21 +0000

    New commit on python input/output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Load, add and Save module.
+
+Contains a script that adds all arguments to a Python
+list, and then save them to a file.
+"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    lst = load_from_json_file("add_item.json")
+except:
+    lst = []
+
+argc = len(sys.argv)
+
+if argc > 1
+    for i in range(1, argc):
+        lst.append(sys.argv)
+
+save_to_json_file(lst, "addd_item.json")
