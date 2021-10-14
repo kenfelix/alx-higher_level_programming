462ca482f110367525d47542f983b3ef42423a14
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-14 09:14:02 +0000

    New commit on python input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""Load, add and Save module.
-
-Contains a script that adds all arguments to a Python
-list, and then save them to a file.
-"""
+"""Add item script."""
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -16,8 +12,8 @@ except:
 
 argc = len(sys.argv)
 
-if argc > 1
+if argc > 1:
     for i in range(1, argc):
         lst.append(sys.argv[i])
 
-save_to_json_file(lst, "addd_item.json")
+save_to_json_file(lst, "add_item.json")
