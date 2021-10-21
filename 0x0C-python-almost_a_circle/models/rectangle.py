4d7cb68d8a7eac0781f1fb4b85fcd9ca3ac9d75b
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-21 18:47:42 +0000

    New commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""The Rectangle Module.
+
+Contains the Class Rectangle that inherits from Base.
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """This class inherits from Base and defines a Rectangle object.
+    Attributes:
+        __width (int): the width of the rectangle.
+        __height (int): the height of the rectangle.
+        __x (int): the horizontal (x) padding of the rectangle.
+        __y (int): the vertical (y) padding of the rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """instantiates the default attribute of the base object"""
+        super.__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Get and Set the width attribute of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get and Set the height attribute of the Rectangle."""
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get and Set the x attribute of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get and Set the y attribute of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__y = value
