afca6f937e0ee41f9f9c5c1d57c6cb346b80e280
Emeka Okafor <kmrapper.kf@gmail.com>
2021-10-26 15:42:54 +0000

    new commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """states the behaviour of the str method"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, 
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
             self.x, self.y, self.width, self.height)
 
     @property
