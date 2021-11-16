657b35540045118ccb1f2e489fb37e98c5605a20
kenfelix <kmrapper.kf@gmail.com>
2021-11-16 13:48:46 -0800

    first commit

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE hbtn_0c_0;
+-- do nothing if the hbtn_0c_0 database already exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
