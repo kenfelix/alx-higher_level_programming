e120eb2b72fcd6f2401db1b8a2f64924d74c8f01
kenfelix <kmrapper.kf@gmail.com>
2021-11-16 14:10:12 -0800

    mySQL Commit

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table called first_table in the current database in your MySQL server.
+-- Do nothing If the table first_table already exists.
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
