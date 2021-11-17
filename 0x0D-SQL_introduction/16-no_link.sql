2128ec22ad4ebd7b8747eac723aaadfd01fbc343
kenfelix <kmrapper.kf@gmail.com>
2021-11-17 02:10:49 -0800

    new commit

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,7 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- Don’t list rows without a name value.
 -- Listed by descending score.
-SELECT `score`, `name`
+SELECT `score`, `name` 
 FROM `second_table`
+WHERE `name` IS NOT NULL
 ORDER BY `score` DESC;
