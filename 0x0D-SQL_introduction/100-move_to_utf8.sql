1f3cecbf766a0b1cdb3fbda560124df4b7942af8
kenfelix <kmrapper.kf@gmail.com>
2021-11-17 02:29:45 -0800

    new commit

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- converts database, table and name field to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+ALTER DATABASE `hbtn_0c_0`
+CHARACTER SET `utf8mb4`
+COLLATE `utf8mb4_unicode_ci`;
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET `utf8mb4`
+COLLATE `utf8mb4_unicode_ci`;
