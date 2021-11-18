0b591415287a7a296ddc56095f1b822c96ee6254
kenfelix <kmrapper.kf@gmail.com>
2021-11-18 13:30:31 -0800

    new commit

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 
 SELECT id, name
 FROM cities
-WHERE state_id IN (SELECT id FROM cities WHERE name = "California")
+WHERE state_id IN (SELECT id FROM states WHERE name = "California")
 ORDER BY id ASC;
