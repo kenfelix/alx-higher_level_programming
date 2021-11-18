ac662b5c42a61c27617fcf248f930cd0691b1a52
kenfelix <kmrapper.kf@gmail.com>
2021-11-18 10:39:38 -0800

    first commit

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Creates the MySQL server user user_0d_1.
+-- User has all privileges on your MySQL server.
+-- Do nothing If the user user_0d_1 already exists.
+
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
