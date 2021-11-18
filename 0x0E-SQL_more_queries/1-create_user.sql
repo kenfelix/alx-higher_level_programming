01c3a837c7294982539fe5c1149af5c3b7d3c1c3
kenfelix <kmrapper.kf@gmail.com>
2021-11-18 10:47:29 -0800

    first commit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 -- User has all privileges on your MySQL server.
 -- Do nothing If the user user_0d_1 already exists.
 
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
