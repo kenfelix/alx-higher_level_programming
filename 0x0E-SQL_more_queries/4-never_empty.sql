17110e2a081df97668e96c776459ebf18ec32a23
kenfelix <kmrapper.kf@gmail.com>
2021-11-18 12:26:56 -0800

    new commit

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- Creates the table id_not_null on your MySQL server.
+-- Do nothing If the table id_not_null already exists.
+
+CREATE TABLE IF NOT EXISTS id_not_null(id INT SET DEFAULT 1, name VARCHAR(256))
