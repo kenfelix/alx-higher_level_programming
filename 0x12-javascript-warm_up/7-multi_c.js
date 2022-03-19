f50cb2ae7180de46259e6aee94cbc87aac54d7b9
kenfelix <kmrapper.kf@gmail.com>
2022-03-19 19:44:45 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 let x = parseInt(process.argv[2]);
 
-while (x !== 0) {
-  console.log('C is fun');
-  x--;
+if (x) {
+  while (x !== 0) {
+    console.log('C is fun');
+    x--;
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
