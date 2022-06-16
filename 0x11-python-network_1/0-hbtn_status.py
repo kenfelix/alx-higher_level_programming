812d8331537662fcfa77455a92bd82baa5608c41
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 15:42:45 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""script that fetches https://intranet.hbtn.io/status"""
+
 import urllib.request
 
-
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        page = response.read()
+
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        res = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(page)))
-        print("\t- content: {}".format(page))
-        print("\t- utf8 content: {}".format(page.decode("utf-8")))
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf-8")))
