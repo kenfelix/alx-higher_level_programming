4e5f131656c6fd9975d955199f9eba14dbfaf39f
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 15:28:05 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""A script that lists all State objects from the database hbtn_0e_6_usa"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
-
-from model_state import Base, State
-
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+via SQLAlchemy
+"""
 if __name__ == "__main__":
-    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
-                           .format(*sys.argv[1:]))
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
-    records = session.query(State).all()
-    for record in records:
-        print("{}: {}".format(record.id, record.name))
+
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for row in session.query(State).order_by(State.id):
+        print("{}: {}".format(row.id, row.name))
