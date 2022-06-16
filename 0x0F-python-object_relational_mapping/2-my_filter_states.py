273ecaefa54f965e341f354ff99215f456f3211b
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 15:22:40 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
+Lists all states from the database hbtn_0e_0_usa where name
+matches the argument
+Takes 4 arguments: mysql username, mysql password, database name,
+and state name searched
 """
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-
-    cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states
-        WHERE CAST(name AS BINARY) = CAST('{}' AS BINARY)
-        ORDER BY states.id;""".format(sys.argv[4]))
-    records = cursor.fetchall()
-    for record in records:
-        print(record)
-    db.close()
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    for row in cur.fetchall():
+        print(row)
