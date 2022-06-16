efeef752f12f6a7794eb0157cc97ae2c1199871b
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 15:31:05 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+
 """
-Contains the class definition of a State that inherits from
-declarative_base
+Module model_state
+Defines State and Base
 """
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Class implementation of the State object"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,
-                nullable=False, unique=True, autoincrement=True)
+    """class State definition
+    id: is primary key
+    name: the state's name
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
